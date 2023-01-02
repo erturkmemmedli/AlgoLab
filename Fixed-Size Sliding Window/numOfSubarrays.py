a27a06c4d960268607a79672d3a035c826908945
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-01-02 21:58:32 +0400

    Create numOfSubarrays.py

---
+++ Fixed-Size Sliding Window/numOfSubarrays.py
@@ -0,0 +1,27 @@
+class Solution:
+    def numOfSubarrays(self, arr, k, threshold):
+        windowSum = sum(arr[:k])
+        count = 1 if windowSum / k >= threshold else 0
+        for i in range(len(arr) - k):
+            windowSum += arr[i + k] - arr[i]
+            if windowSum / k >= threshold:
+                count += 1
+        return count
+      
+# Official solution given
+
+class Solution:
+    def numOfSubarrays(self, arr, k, threshold):
+        count = 0
+        windowSum = sum(arr[:k])
+
+        if windowSum / k >= threshold:
+            count += 1
+
+        for i in range(len(arr) - k):
+            windowSum = windowSum - arr[i] + arr[i + k]
+            average = windowSum / k
+            if average >= threshold:
+                count += 1
+
+        return count
