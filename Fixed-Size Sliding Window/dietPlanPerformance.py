fee550dcf666fe251c447d1905c35b64eeec77a5
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-01-02 22:11:05 +0400

    Create dietPlanPerformance.py

---
+++ Fixed-Size Sliding Window/dietPlanPerformance.py
@@ -0,0 +1,32 @@
+class Solution:
+    def dietPlanPerformance(self, calories, k, lower, upper):
+        calory = sum(calories[:k])
+        point = 1 if calory > upper else -1 if calory < lower else 0
+        for i in range(len(calories) - k):
+            calory += calories[i + k] - calories[i]
+            if calory > upper:
+                point += 1
+            elif calory < lower:
+                point -= 1
+        return point
+      
+# Official solution given
+
+class Solution:
+    def dietPlanPerformance(self, calories, k, lower, upper):
+        windowSum = sum(calories[:k])
+        points = 0
+
+        if windowSum < lower:
+            points -= 1
+        if windowSum > upper:
+            points += 1
+
+        for i in range(len(calories) - k):
+            windowSum = windowSum - calories[i] + calories[i + k]
+            if windowSum < lower:
+                points -= 1
+            if windowSum > upper:
+                points += 1
+
+        return points
