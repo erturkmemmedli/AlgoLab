39a35a335c6b7239eb8f7e77d5193dd0c3c656f1
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-01-10 16:59:30 +0400

    Update lengthOfLongestSubstring.py

--- Dynamic-Size Sliding Window/lengthOfLongestSubstring.py
+++ Dynamic-Size Sliding Window/lengthOfLongestSubstring.py
@@ -15,3 +15,21 @@ class Solution:
                         del hashmap[s[left]]
                     left += 1
         return length
+
+# Official solution
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        commonChars = set()
+        windowStart = 0
+        windowEnd = 0
+        length = 0
+
+        while windowEnd < len(s):
+            while s[windowEnd] in commonChars:
+                commonChars.remove(s[windowStart])
+                windowStart += 1
+            commonChars.add(s[windowEnd])
+            length = max(length, windowEnd - windowStart + 1)
+            windowEnd += 1
+        return length
