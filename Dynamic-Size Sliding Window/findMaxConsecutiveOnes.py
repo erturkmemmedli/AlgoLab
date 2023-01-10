cfea8be02d7ba6a86253d2be8396d05bd99b0e1b
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-01-10 16:58:21 +0400

    Update findMaxConsecutiveOnes.py

--- Dynamic-Size Sliding Window/findMaxConsecutiveOnes.py
+++ Dynamic-Size Sliding Window/findMaxConsecutiveOnes.py
@@ -8,3 +8,18 @@ class Solution:
             else:
                 left = right
         return maxOnes
+
+# Official solution
+
+class Solution:
+    def findMaxConsecutiveOnes(self, nums):
+        maxLength = 0
+        windowStart = 0
+
+        for windowEnd in range(len(nums)):
+            if nums[windowEnd] == 0:
+                windowStart = windowEnd + 1
+            else:
+                currentLength = windowEnd - windowStart + 1
+                maxLength = max(maxLength, currentLength)
+        return maxLength
