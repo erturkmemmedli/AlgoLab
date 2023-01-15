6597fa64a051af2927f5b0504a70d9d55cd9053f
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-01-15 13:16:01 +0400

    Update search.py

--- Modified Binary Search/search.py
+++ Modified Binary Search/search.py
@@ -10,3 +10,21 @@ class Solution:
             else:
                 left = mid + 1
         return -1
+
+# Official solution
+
+class Solution:
+    def search(self, nums, target):
+        start = 0
+        end = len(nums) - 1
+
+        while start <= end:
+            middle = start + (end - start) // 2
+            if nums[middle] == target:
+                return middle
+            if target < nums[middle]:
+                end = middle - 1
+            else:
+                start = middle + 1
+
+        return -1
