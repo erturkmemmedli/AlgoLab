769918312caf7a82816c840b9675e83eb43bac29
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-06 15:21:04 +0400

    Update lengthOfLongestSubstringTwoDistinct.py

--- 3 - Dynamic-Size Sliding Window/lengthOfLongestSubstringTwoDistinct.py
+++ 3 - Dynamic-Size Sliding Window/lengthOfLongestSubstringTwoDistinct.py
@@ -18,3 +18,36 @@ class Solution:
                     left += 1
                 window[s[right]] = 1
         return longest
+
+# Official solution
+
+class Solution:
+    def lengthOfLongestSubstringTwoDistinct(self, s: str) -> int:
+        n = len(s)
+        if n < 3:
+            return n
+
+        windowStart = 0
+        charMap = {}
+        maxLen = 0
+        
+        for windowEnd in range(n):
+            char = s[windowEnd]
+
+            if char not in charMap:
+                charMap[char] = 1
+
+                while len(charMap) > 2:
+                    charToRemove = s[windowStart]
+                    charMap[charToRemove] -= 1
+
+                    if charMap[charToRemove] == 0:
+                        del charMap[charToRemove]
+
+                    windowStart += 1
+            else:
+                charMap[char] += 1
+                
+            maxLen = max(maxLen, windowEnd - windowStart + 1)
+            
+        return maxLen
