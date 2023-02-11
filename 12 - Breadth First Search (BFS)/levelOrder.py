4304ff9bae92dc5243e96bc231ebf7d6a8075797
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-11 19:39:07 +0400

    Create levelOrder.py

---
+++ 12 - Breadth First Search (BFS)/levelOrder.py
@@ -0,0 +1,55 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from collections import deque
+
+class Solution:
+    def levelOrder(self, root):
+        if not root:
+            return []
+        queue = deque([root])
+        levelOrder = []
+        while queue:
+            levelSize = len(queue)
+            level = []
+            for _ in range(levelSize):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            levelOrder.append(level)
+        return levelOrder
+        
+# Official solution
+
+class Solution:
+    def levelOrder(self, root):
+        if root is None:
+            return []
+
+        queue = [root]
+        output = []
+
+        while queue:
+            levelSize = len(queue)
+            levelTraversal = []
+
+            for _ in range(levelSize):
+                currentNode = queue.pop(0)
+                levelTraversal.append(currentNode.val)
+
+                if currentNode.left:
+                    queue.append(currentNode.left)
+
+                if currentNode.right:
+                    queue.append(currentNode.right)
+
+            output.append(levelTraversal)
+
+        return output
