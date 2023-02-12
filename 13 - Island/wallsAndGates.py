c6eb44359ca0d948de68d9cbc7ca9d62fd99a840
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-12 19:54:01 +0400

    Update wallsAndGates.py

--- 13 - Island/wallsAndGates.py
+++ 13 - Island/wallsAndGates.py
@@ -48,6 +48,39 @@ class Solution:
             self.dfs(rooms, m, n, row, col - 1, rooms[row][col], visited)
             self.dfs(rooms, m, n, row, col + 1, rooms[row][col], visited)
             
+# Alternative solution
+
+from collections import deque
+
+class Solution:
+    def wallsAndGates(self, rooms):
+        """
+        Do not return anything, modify rooms in-place instead.
+        """
+        m = len(rooms)
+        n = len(rooms[0])
+        gates = []
+        for i in range(m):
+            for j in range(n):
+                if rooms[i][j] == 0:
+                    gates.append((i, j))
+        for r, c in gates:
+            for row, col in [r-1, c], [r, c-1], [r+1, c], [r, c+1]:
+                if m > row >= 0 <= col < n  and rooms[row][col] != -1 and rooms[row][col] != 0:
+                    self.bfs(rooms, m, n, row, col)
+                
+    def bfs(self, rooms, m, n, row, col):
+        queue = deque([(row, col)])
+        rooms[row][col] = 1 
+        visited = {(row, col)}
+        while queue:
+            r, c = queue.popleft()
+            for i, j in [r-1, c], [r, c-1], [r+1, c], [r, c+1]:
+                if m > i >= 0 <= j < n and rooms[i][j] != -1 and rooms[i][j] != 0 and (i, j) not in visited:
+                    rooms[i][j] = min(rooms[i][j], rooms[r][c] + 1)
+                    queue.append((i, j))
+                    visited.add((i, j))
+            
 # Official solution
 
 class Solution:
