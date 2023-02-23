0f682275e16b364834fd78ebd33b6aa81fed743c
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-02-23 15:07:19 +0400

    Update suggestedProducts.py

--- 15 - Tries/suggestedProducts.py
+++ 15 - Tries/suggestedProducts.py
@@ -47,3 +47,57 @@ class Solution:
                 output.append(suggestion)
 
         return output
+
+# Official solution
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.searchWords = []
+        
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, data):
+        node = self.root
+        idx = 0
+
+        for char in data:
+            idx += 1
+
+            if char not in node.children:
+                node.children[char] = TrieNode()
+
+            node = node.children[char]
+
+            if len(node.searchWords) < 3:
+                node.searchWords.append(data)
+    
+    def search(self, prefix):
+        node = self.root
+
+        for char in prefix:
+            if char not in node.children:
+                return []
+
+            node = node.children[char]
+
+        return node.searchWords
+
+class Solution:
+    def suggestedProducts(self, products, searchWord):
+        products.sort()
+        trie = Trie()
+        
+        for product in products:
+            trie.insert(product)
+        
+        result = []
+        prefix = ""
+
+        for char in searchWord:
+            prefix += char
+            result.append(trie.search(prefix))
+
+        return result
