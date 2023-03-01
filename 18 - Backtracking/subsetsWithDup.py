7d3ba31f371e39296da2d1af62a829ee4a0b652c
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-03-01 11:21:34 +0400

    Create subsetsWithDup.py

---
+++ 18 - Backtracking/subsetsWithDup.py
@@ -0,0 +1,37 @@
+class Solution:
+    def subsetsWithDup(self, nums):
+        nums.sort()
+        self.result = []
+        self.backtrack(nums, [], 0)
+        return self.result
+
+    def backtrack(self, nums, currState, start):
+        if currState not in self.result:
+            self.result.append(currState)
+        for i in range(start, len(nums)):
+            self.backtrack(nums, currState + [nums[i]], i + 1)
+            
+# Official solution
+
+class Solution:
+    def subsetsWithDup(self, nums):
+        result = []
+
+        nums.sort()
+
+        self.backtrack(nums, [], result, 0)
+
+        return result
+    
+    def backtrack(self, nums, path, result, start):
+        result.append(path[:])
+
+        for i in range(start, len(nums)):
+            if i > start and nums[i] == nums[i-1]:
+                continue
+
+            path.append(nums[i])
+
+            self.backtrack(nums, path, result, i + 1)
+
+            path.pop()
