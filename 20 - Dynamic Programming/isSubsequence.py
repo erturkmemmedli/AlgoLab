ce1302702ba1f296cf0e7d6101c0405f6f61c982
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-03-10 12:10:33 +0400

    Create isSubsequence.py

---
+++ 20 - Dynamic Programming/isSubsequence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        dp = [[0 for _ in range(len(t) + 1)] for _ in range(len(s) + 1)]
+        for i in range(1, len(s) + 1):
+            for j in range(1, len(t) + 1):
+                if s[i - 1] == t[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
+        return dp[-1][-1] == len(s)
