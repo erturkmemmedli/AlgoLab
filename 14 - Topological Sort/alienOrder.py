b61389edc8c3de925b14b19fe0b5ce61e7c8324e
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-05-27 01:40:21 +0400

    Update alienOrder.py

--- 14 - Topological Sort/alienOrder.py
+++ 14 - Topological Sort/alienOrder.py
@@ -37,67 +37,53 @@ words[i] consists of only lowercase English letters.
 from collections import deque
 
 class Solution:
-    def alienOrder(self, words):        
+    def alienOrder(self, words: List[str]) -> str:
         self.letters = set()
         
         for word in words:
             for letter in word:
                 self.letters.add(letter)
+
+        self.graph = defaultdict(set)
+        self.indegree = {char : 0 for char in self.letters}
         
-        self.alienDictionary = {letter: set() for letter in self.letters}        
-        self.inDegree = {letter: 0 for letter in self.letters}
-        self.violation = False
-        
-        self.checkLetters(words)
-        
-        if self.violation:
-            return ""
-        
-        queue = deque([node for node, degree in self.inDegree.items() if degree == 0])
-        alienAlphabet = []
+        for i in range(1, len(words)):
+            if not self.compareWords(words[i-1], words[i]):
+                return ""
+
+        queue = deque([key for key, val in self.indegree.items() if val == 0])
+        alien_dictionary = []
         
         while queue:
             node = queue.popleft()
-            alienAlphabet.append(node)
+            alien_dictionary.append(node)
             
-            for neighbor in self.alienDictionary[node]:
-                self.inDegree[neighbor] -= 1
+            for neighbor in self.graph[node]:
+                self.indegree[neighbor] -= 1
                 
-                if self.inDegree[neighbor] == 0:
+                if self.indegree[neighbor] == -1:
+                    return ""
+                    
+                if self.indegree[neighbor] == 0:
                     queue.append(neighbor)
-                
-        return "".join(alienAlphabet) if len(alienAlphabet) == len(self.letters) else ""
+                    
+        return "".join(alien_dictionary) if len(alien_dictionary) == len(self.graph) else ""
+        
+    def compareWords(self, a, b):
+        i = 0
+        
+        while i < min(len(a), len(b)):
+            if a[i] != b[i]:
+                if b[i] in self.graph[a[i]]:
+                    return True
 
-    def checkLetters(self, words):
-        if not words:
-            return
-       
-        if not words[0]:
-            return self.checkLetters(words[1:])
-         
-        if any(word == "" for word in words):
-            self.violation = True
-            return
-        
-        check = [words[0]]
-        
-        for i in range(1, len(words)):            
-            if words[i][0] == check[-1][0]:
-                check.append(words[i])
-                
-            else:
-                if len(check) > 1:
-                    self.checkLetters([word[1:] for word in check])
-                    
-                    if self.violation:
-                        return
-                    
-                self.alienDictionary[check[-1][0]].add(words[i][0])
-                self.inDegree[words[i][0]] += 1
-                check = [words[i]]
-                
-        if len(check) > 1:
-            self.checkLetters([word[1:] for word in check])
+                self.graph[a[i]].add(b[i])
+                self.indegree[b[i]] += 1
+                return True
+            
+            i += 1
+
+        return len(a) <= len(b)
             
 # Official solution
 
